22381d39fde05a6dabcf8034795e1410ebda6b5a
jeffigy <evangelistajeffy25@gmail.com>
2022-03-08 00:18:57 -0800

    iframe

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,24 +5,18 @@ import {
 } from '@chakra-ui/react'
 import React from 'react'
 import { Layout } from '../components/Layout'
+import IframeResizer from "iframe-resizer-react";
 
 export default function Homepage() {
   return (
     <Layout>
-      <Stack
-        as={Box}
-        textAlign={'center'}
-        spacing={{ base: 8, md: 14 }}
-        py={{ base: 20, md: 36 }}>
-        <Heading
-          fontWeight={800}
-          fontSize={{ base: '2xl', sm: '4xl', md: '6xl' }}
-          lineHeight={'110%'}
-          color="blue.500"
-        >
-          e-Maternal Care and Services
-        </Heading>
-      </Stack>
+
+        <IframeResizer
+                log
+                src="https://gulaneskorp.betteruptime.com/"
+                style={{ width: '1px', minWidth: '100%', minHeight: '700px'}}
+            />
+     
     </Layout>
 
   )
