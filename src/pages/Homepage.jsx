9bba97189ea1b060332ff9b55260ba98b94fb3d9
jeffigy <evangelistajeffy25@gmail.com>
2022-08-12 10:45:54 +0800

    first try

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,24 +1,21 @@
-import {
-  Heading,
-  Stack,
-  Box, Link, Text
-} from '@chakra-ui/react'
-import React from 'react'
-import { Layout } from '../components/Layout'
+import { Heading, Stack, Box, Link, Text, AspectRatio } from "@chakra-ui/react";
+import React from "react";
+import { Layout } from "../components/Layout";
 import IframeResizer from "iframe-resizer-react";
 
 export default function Homepage() {
   return (
     <Layout>
+      <AspectRatio maxW="1920px" ratio={1}>
+        {/* <iframe
+          title="naruto"
+          src="https://drive.google.com/file/d/1rXnT32G4uzVyDN9zeNeFqz-csdTxe_45/preview"
+          allowFullScreen
+          allowAutoPlay
+        /> */}
+        <iframe src="https://drive.google.com/file/d/1rXnT32G4uzVyDN9zeNeFqz-csdTxe_45/preview" width="640" height="480" allow="autoplay"></iframe>
 
-        <IframeResizer
-                log
-                src="https://gulaneskorp.betteruptime.com/"
-                style={{ width: '1px', minWidth: '100%', minHeight: '700px'}}
-            />
-     
+      </AspectRatio>
     </Layout>
-
-  )
+  );
 }
-
